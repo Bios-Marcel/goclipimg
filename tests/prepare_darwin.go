c80810104ff9dc036da47ee58a2fc65a415365eb
Marcel Schramm <marceloschr@googlemail.com>
2019-02-12 11:55:30 +0100

    Capture and return error output of osascript

--- tests/prepare_darwin.go
+++ tests/prepare_darwin.go
@@ -1,10 +1,48 @@
 package tests
 
-import "os/exec"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os/exec"
+)
 
 const imageName = "image.png"
 const osaScript = "set the clipboard to (read (POSIX file '" + imageName + "') as «class PNGf»)"
 
 func fillClipboard() error {
-	return exec.Command("osascript", "-e", osaScript).Run()
+	command := exec.Command("osascript", "-e", osaScript)
+
+	errorPipe, err := command.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	outputPipe, err := command.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	commandError := command.Start()
+	if commandError != nil {
+		return commandError
+	}
+
+	errScanner := bufio.NewScanner(errorPipe)
+	errOut := ""
+	for errScanner.Scan() {
+		errOut = errOut + errScanner.Text()
+	}
+
+	outScanner := bufio.NewScanner(outputPipe)
+	out := ""
+	for outScanner.Scan() {
+		out = out + outScanner.Text()
+	}
+
+	if errOut != "" || out != "" {
+		return errors.New(fmt.Sprintf("%s | %s", errOut, out))
+	}
+
+	return nil
 }
