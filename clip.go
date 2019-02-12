92e8f6029fd8060bc6da937f64f00a314fcda8f7
Marcel Schramm <marceloschr@googlemail.com>
2019-02-12 08:41:56 +0100

    Fix typo in error message

--- clip.go
+++ clip.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	// ErrImagePasteUnsupported means that xclip can't be found or isn't installed.
-	ErrImagePasteUnsupported = errors.New("xclip is nto available on this system")
+	ErrImagePasteUnsupported = errors.New("xclip is not available on this system")
 	// ErrNoImageInClipboard means that no data was returned.
 	ErrNoImageInClipboard = errors.New("the clipboard doesn't contain an image")
 )
