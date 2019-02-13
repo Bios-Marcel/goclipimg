fbd7538ff3acccf6f3fc6687181f41ea46bf6c5a
Marcel Schramm <marceloschr@googlemail.com>
2019-02-13 10:36:02 +0100

    Add check if clipboard contains image

--- clip_darwin.go
+++ clip_darwin.go
@@ -7,19 +7,36 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 const imageToFile = "tell application \"System Events\" to write (the clipboard as «class PNGf») to \"%s\""
+const containsImage = "if ((clipboard info) as string) does not contain \"«class PNGf»\" then error number 61 end if"
 
 // ErrImagePasteUnsupported means that pngpaste can't be found or isn't installed.
 var ErrImagePasteUnsupported = errors.New("pngpaste can't be found on this system")
 
-func isCommandAvailable(name string) bool {
-	_, fileError := exec.LookPath(name)
-	return fileError == nil
-}
-
 func getImageFromClipboard() ([]byte, error) {
+	containsImageCommand := exec.Command("osascript", "-e", containsImage)
+
+	containsCheckError := containsImageCommand.Start()
+	if containsCheckError != nil {
+		return nil, containsCheckError
+	}
+
+	waitForContainsCheckError := containsImageCommand.Wait()
+	if waitForContainsCheckError != nil {
+		if exitError, ok := waitForContainsCheckError.(*exec.ExitError); ok {
+			if status, ok := exitError.Sys().(syscall.WaitStatus); ok {
+				if status == 61 {
+					return nil, ErrNoImageInClipboard
+				}
+			}
+		} else {
+			return nil, waitForContainsCheckError
+		}
+	}
+
 	tempFile, tempFileError := ioutil.TempFile("", "clipimg")
 	if tempFileError != nil {
 		return nil, tempFileError
