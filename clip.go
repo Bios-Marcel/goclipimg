6ad175b6f5f817e0e2ee26cb7b58faeaddc8e0eb
Marcel Schramm <marceloschr@googlemail.com>
2019-02-13 06:46:05 +0100

    Add comments

--- clip.go
+++ clip.go
@@ -6,11 +6,29 @@ import (
 )
 
 var (
-	pngHeader = []byte{0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A}
+	pngHeader = []byte{
+		/* Has the high bit set to detect transmission systems that do not
+		support 8 bit data and to reduce the chance that a text file i
+		mistakenly interpreted as a PNG, or vice versa. */
+		0x89,
+		/* In ASCII, the letters PNG, allowing a person to identify the
+		   format easily if it is viewed in a text editor. */
+		0x50, 0x4E, 0x47,
+		/* A DOS-style line ending (CRLF) to detect DOS-Unix line ending
+		   conversion of the data.  */
+		0x0D, 0x0A,
+		/* A byte that stops display of the file under DOS when the command
+		   type has been used—the end-of-file character.  */
+		0x1A,
+		/* A Unix-style line ending (LF) to detect Unix-DOS line ending
+		   conversion.  */
+		0x0A}
 	// ErrNoImageInClipboard means that no data was returned.
 	ErrNoImageInClipboard = errors.New("the clipboard doesn't contain an image")
 )
 
+// GetImageFromClipboard returns either a byte array containing PNG data or an
+// error that indicates that no png could be retrieved.
 func GetImageFromClipboard() ([]byte, error) {
 	data, err := getImageFromClipboard()
 	if err != nil {
--- clip_unsupported.go
+++ clip_unsupported.go
@@ -7,7 +7,7 @@ import "errors"
 // ErrImagePasteUnsupported means that this system has no implementation for pasting an image.
 var ErrImagePasteUnsupported = errors.New("This system doesn't have a paste implementation.")
 
-// GetImageFromClipboard always returns ErrImagePasteUnsupported, since the
+// getImageFromClipboard always returns ErrImagePasteUnsupported, since the
 // compilation target currently doesn't support pasting images.
 func getImageFromClipboard() ([]byte, error) {
 	return nil, ErrImagePasteUnsupported
